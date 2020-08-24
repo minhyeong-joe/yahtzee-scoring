0961b27146a955d7a53fb4ad844733da8358d153
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2020-08-24 16:40:58 -0700

    finished basic scoring logic

---
+++ README.md
@@ -0,0 +1,9 @@
+# Yahtzee Logic
+
+Scoring estimating logic for [Yahtzee Game](https://www.dicegamedepot.com/yahtzee-rules/)
+
+This will likely be integrated into mobile Yahtzee Scoring Card, where each user can fill in his/her own score.
+
+# Run App
+
+To run, use `Node index.js`
---
+++ index.js
@@ -0,0 +1,131 @@
+const readline = require('readline');
+
+// constant scores
+const FULL_HOUSE = 25;
+const SM_STRAIGHT = 30;
+const LG_STRAIGHT = 40;
+const YAHTZEE = 50;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// get dices from input
+rl.question("Enter the eyes of five dices separated by space: ", answer => {
+    dices = answer.split(" ").map(dice => parseInt(dice));
+    if (!isValidDices(dices)) {
+        console.log("Invalid input");
+    } else {
+        console.log(dices);
+        computeScore(dices);
+    }
+    rl.close();
+});
+
+// check if input dices are valid
+// valid iff (1)there are 5 dices; (2)each dice is an integer between 1 and 6 (inclusive)
+const isValidDices = dices => {
+    if (dices.length != 5) {
+        return false;
+    }
+    for (const dice of dices) {
+        if (dice < 1 || dice > 6 || !Number.isInteger(dice)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+// compute Score given valid 5 dices
+const computeScore = dices => {
+    // sort for straight check
+    dices.sort();
+    console.log("Sorted:", dices);
+    // freq for dice counter
+    const freq = {};
+    const sumOfEyes = dices.reduce((acc, dice)=>acc+dice, 0);
+    const scores = {
+        ones: 0,
+        twos: 0,
+        threes: 0,
+        fours: 0,
+        fives: 0,
+        sixs: 0,
+        threeOfKind: 0,
+        fourOfKind: 0,
+        fullHouse: 0,
+        smStraight: 0,
+        lgStraight: 0,
+        yahtzee: 0,
+        choice: 0
+    };
+    // compute singles & count occurrences at the same time
+    dices.forEach(dice => {
+        if (freq[dice]) {
+            freq[dice]++;
+        } else {
+            freq[dice] = 1;
+        }
+        switch (dice) {
+            case 1:
+                scores.ones++;
+                break;
+            case 2:
+                scores.twos+=2;
+                break;
+            case 3:
+                scores.threes+=3;
+                break;
+            case 4:
+                scores.fours+=4;
+                break;
+            case 5:
+                scores.fives+=5;
+                break;
+                case 6:
+                    scores.sixs+=6;
+                    break;
+                }
+            });
+            // compute 3 of a kind & 4 of a kind & Full House & Yahtzee
+    console.log("Count:", freq);
+    for (const [_, count] of Object.entries(freq)) {
+        if (count >= 3) {
+            scores.threeOfKind = sumOfEyes;
+            // full house is special type of 3 of a kind
+            if (count == 3 && Object.keys(freq).length == 2) {
+                scores.fullHouse = FULL_HOUSE;
+            }
+        }
+        if (count >= 4) {
+            scores.fourOfKind = sumOfEyes;
+        }
+        if (count == 5) {
+            scores.yahtzee = YAHTZEE;
+        }
+    }
+    // compute SM straight & LG straight
+    let straight = 1;
+    let maxStraight = 1;
+    for (let i = 1; i < dices.length; i++) {
+        if (dices[i] == dices[i-1]+1) {
+            straight++;
+        } else if (dices[i] > dices[i-1]+1) {
+            straight = 1;
+        }
+        maxStraight = Math.max(straight, maxStraight);
+    }
+    if (maxStraight >= 4) {
+        scores.smStraight = SM_STRAIGHT;
+    }
+    if (maxStraight == 5) {
+        scores.lgStraight = LG_STRAIGHT;
+    }
+
+    // choice score is just sum of all dices
+    scores.choice = sumOfEyes;
+
+    console.log(scores);
+};
+
